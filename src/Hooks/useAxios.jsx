c843307f0dfcb7307f5fa556501cc0f8dfb6522e
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-10-17 00:24:13 -0500

    Bregoli

--- src/Hooks/useAxios.jsx
+++ src/Hooks/useAxios.jsx
@@ -1,27 +1,30 @@
 import axios from "axios";
-import React, { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback } from "react";
 
-const useAxios = ({ url }) => {
-  const [data, setData] = useState([]);
+const useAxios = (url) => {
+  const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  useEffect(() => {
-    const getApiData = async () => {
-      setLoading(true);
-      try {
-        const result = await axios.get(url);
-        setData(result.data);
-        console.log(result.data);
-      } catch (err) {
-        setError(err.message);
-        console.log(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-    getApiData();
+  const getApiData = useCallback(async () => {
+    setLoading(true);
+    try {
+      const result = await axios.get(url);
+      setData(result.data);
+      console.log("Axios Hook data: ");
+      console.table(result.data);
+    } catch (err) {
+      setError(err.message);
+      console.log(err.message);
+    } finally {
+      setLoading(false);
+    }
   }, [url]);
+
+useEffect(() => {
+  getApiData();
+}, [getApiData]);
+
   return { data, loading, error };
 };
 
--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,24 +1,56 @@
-import React from 'react'
-import Styles from "../../Styles/Page-Styles/Landing/Landing.module.css"
+import React from "react";
+import { CSSProperties } from "react";
+import Styles from "../../Styles/Page-Styles/Landing/Landing.module.css";
+import ClipLoader from "react-spinners/ClipLoader";
+import ScaleLoader from "react-spinners/ScaleLoader";
 
-import HeroBoardSlide from '../../Components/Swiper/HeroBoardSlide'
-import PlaceholderSlide from '../../Components/Swiper/PlaceholderSlide'
-import MensDataSlider from '../../Components/Swiper/MensDataSlider'
-import WomensDataSlider from '../../Components/Swiper/WomensDataSlider'
-import ElectronicsDataSlider from '../../Components/Swiper/ElectronicsDataSlider'
-import JeweleryDataSlider from '../../Components/Swiper/JeweleryDataSlider'
+import HeroBoardSlide from "../../Components/Swiper/HeroBoardSlide";
+import PlaceholderSlide from "../../Components/Swiper/PlaceholderSlide";
+import MensDataSlider from "../../Components/Swiper/MensDataSlider";
+import WomensDataSlider from "../../Components/Swiper/WomensDataSlider";
+import ElectronicsDataSlider from "../../Components/Swiper/ElectronicsDataSlider";
+import JeweleryDataSlider from "../../Components/Swiper/JeweleryDataSlider";
+
+import useAxios from "../../Hooks/useAxios";
 
 const Landing = () => {
+  const url = `https://fakestoreapi.com/products/category/electronics`;
+  const { data, loading, error } = useAxios(url);
+
+  const override  = {
+    display: "block",
+    margin: "0 auto",
+    borderColor: "red",
+  };
+
   return (
     <div className={Styles.Page}>
       <HeroBoardSlide />
+      <div className={Styles.Temporary}>
+        <h1>useAxios Hook Data:</h1>
+        {loading && (
+          <ScaleLoader
+            color={"red"}
+            loading={loading}
+            cssOverride={override}
+            size={80}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        )}
+        <p>{data && data[1]?.title}</p>
+        <p>{data && data[1]?.category}</p>
+        <p>{data && data[1]?.description}</p>
+        <img src={data && data[1]?.image} alt="" />
+        {error && <h3>Something went wrong..</h3>}
+      </div>
       <PlaceholderSlide />
       <MensDataSlider />
       <WomensDataSlider />
       <ElectronicsDataSlider />
       <JeweleryDataSlider />
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
